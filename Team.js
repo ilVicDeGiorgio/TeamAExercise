922a143cbbad038beaa4f6b4bcaf5cb9bff1a595
Vittorio De Giorgio <134387179+ilVicDeGiorgio@users.noreply.github.com>
2024-02-07 13:55:11 +0100

    Update Team.js

--- Team.js
+++ Team.js
@@ -1,16 +1,1 @@
 const team = [];
-
-const student1 = {
-  name: "Vittorio",
-  surname: "De Giorgio",
-  age: 27,
-  city: "Amaroni",
-  hobby: "Gym",
-  favoriteFood: "Carbonara",
-  favoriteVideoGame: "DaysGone",
-  favoriteFilm: "Interstellar",
-  favoriteBook: "",
-  petName: "Dog",
-};
-
-team.push(student1);
