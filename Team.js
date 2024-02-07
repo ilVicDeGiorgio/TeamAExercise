8bd2a2a6b3247fdb1c6ab97c3dbd29af32b1273c
Vittorio De Giorgio <vdegiorgio8@gmail.com>
2024-02-07 14:04:16 +0100

    Create student Vittorio

--- Team.js
+++ Team.js
@@ -1,1 +1,16 @@
 const team = [];
+
+const student = {
+  name: "Vittorio",
+  surname: "De Giorgio",
+  age: 27,
+  city: "Amaroni",
+  hobby: "Gym",
+  favouriteFood: "Carbonara",
+  favouriteVideogame: "Daysgone",
+  favouriteFilm: "Interstellar",
+  favouriteBook: "Adrenalina",
+  petName: "Doggo",
+};
+
+team.push(student);
