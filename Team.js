441d278190740e8ef438e0a008cf2613cfcdea44
Rossella Sonzogni <ross.sonzogni@gmail.com>
2024-02-09 10:07:58 +0100

    aggiunto variabile studen3 e pushata nell'array

--- Team.js
+++ Team.js
@@ -13,4 +13,17 @@ const student1 = {
   petName: "Dog",
 };
 
-team.push(student1);
+const student3 = {
+  name: "Rossella",
+  surname: "Sonzogni",
+  age: 30,
+  city: "Anzio",
+  hobby: "Cooking",
+  favoriteFood: "Pizza",
+  favoriteVideoGame: "The Witcher",
+  favoriteFilm: "V per Vendetta",
+  favoriteBook: "Io e te, Nicola Ammaniti",
+  petName: "Pizzetta",
+};
+
+team.push(student1, student3);
