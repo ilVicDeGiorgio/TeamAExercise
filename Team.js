74b4b7f3a281968f0eab4af464cf7159f07bbdb0
Henvil <turningsheeps@gmail.com>
2024-02-12 22:21:45 +0100

    added student2 (letterio) in Team.js

--- Team.js
+++ Team.js
@@ -1,16 +1,16 @@
 const team = [];
 
-const student = {
-  name: "Vittorio",
-  surname: "De Giorgio",
-  age: 27,
-  city: "Amaroni",
-  hobby: "Gym",
-  favouriteFood: "Carbonara",
-  favouriteVideogame: "Daysgone",
-  favouriteFilm: "Interstellar",
-  favouriteBook: "Adrenalina",
-  petName: "Doggo",
+const student2 = {
+  name: "Letterio",
+  surname: "Briguglio",
+  age: 28,
+  city: "Messina",
+  hobby: "Roleplaying",
+  favouriteFood: "Lasagne",
+  favouriteVideogame: "Metal gear solid 3",
+  favouriteFilm: "Everything Everywhere All at Once",
+  favouriteBook: "NON-A by A. Van Vogt",
+  petName: "JoJo",
 };
 
-team.push(student);
+team.push(student2);
