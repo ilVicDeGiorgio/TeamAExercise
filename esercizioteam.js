fdbf261a020ef909bf0835d81e3a46e62ee30c74
massimo altamura <maltamura494@gmail.com>
2024-02-26 22:38:30 +0100

    "esercizio coretto"

--- esercizioteam.js
+++ esercizioteam.js
@@ -1,12 +1,12 @@
-function completeTask(tasks, index, callback) {
+function completeTask(task, index, callback) {
     // Simulazione di un'operazione asincrona utilizzando setTimeout
     setTimeout(() => {
-      if (index < 0 || index >= tasks.length) {
+      if (index < 0 || index >= task.length) {
         // Se l'indice non è nell'intervallo valido, richiama la callback con un oggetto Error
         callback(new Error("Indice attività non valido"));
       } else {
         // Rimuovi l'attività all'indice specificato
-        tasks.splice(index, 1);
+        task.splice(index, 1);
         // Richiama la callback con null (nessun errore) e il messaggio di successo
         callback(null, "Attività completata correttamente");
       }
